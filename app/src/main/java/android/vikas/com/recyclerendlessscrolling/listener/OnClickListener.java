84148d3603ea3e5867827bdabed6d30d7911ac4e
worstkiller <vkbookworm@gmail.com>
2016-12-22 19:00:11 +0530

    style: Change formatting for onclicklistener

---
+++ app/src/main/java/android/vikas/com/recyclerendlessscrolling/listener/OnClickListener.java
@@ -0,0 +1,12 @@
+package android.vikas.com.recyclerendlessscrolling.listener;
+
+import android.view.View;
+
+/**
+ * Created by OFFICE on 12/15/2016.
+ */
+
+public interface OnClickListener {
+
+    public void onClick(View view, int position);
+}
